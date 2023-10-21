732f0f20682b01cea0e77ae1df3c636ea9be5fd7
Jimi Huard <jimi@Jimis-MacBook-Pro.local>
2023-10-20 23:44:40 -0700

    Client file read functional

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 
+use std::any::type_name;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
@@ -124,6 +125,12 @@ fn handle_ping_cmd(config: &JsonValue, stream: &mut TcpStream) {
 
 fn handle_file_read_cmd(config: &JsonValue, stream: &mut TcpStream) {
     log!("Handling file read command");
+
+    if config["properties"]["file_read"] != true {
+        log!("Rejecting file read command as read property is disabled.");
+        let _ = stream.write(&NACK_SEQUENCE);
+        return;
+    }
 
     match validate_key(get_key_type(config), &String::from(config["net_config"]["key"].as_str().unwrap()), stream) {
         true => {
@@ -265,7 +272,227 @@ fn handle_incoming_stream(config: &JsonValue, stream : &mut TcpStream) {
     
 }
 
+fn client_connect(config : &JsonValue) -> Result<TcpStream, std::io::Error> {
+    let server = format!("{}:{}", config["net_config"]["ip"], config["net_config"]["port"]);
+    log!("Attempting connection to {server}");
+    
+    TcpStream::connect(server)
+}
+
+fn client_send_key(config: &JsonValue, stream : &mut TcpStream) -> Result<usize, std::io::Error> {
+    match get_key_type(config) {
+        KeyType::Fixed => {
+            let key = config["net_config"]["key"].to_string();
+            log!("Sending fixed keytype {key}");
+            
+            if key.len() == 0 {
+                return Ok(0);
+            }
+
+            stream.write(&key.as_bytes())
+        },
+
+        KeyType::Invalid => {
+            log!("Invalid key type detected in config.");
+            return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Bad key type"));
+        }
+    }
+}
+
+fn handle_client_write(args: &Vec<String>, config : &JsonValue) {
+    if args.len() != 4 {
+        print_client_usages();
+        return;
+    }
+}
+
+fn handle_client_read(args: &Vec<String>, config : &JsonValue) {
+    if args.len() != 4 {
+        print_client_usages();
+        return;
+    }
+
+    let remote_source_file = &args[2];
+    let local_destination_file = &args[3];
+
+    match client_connect(config) {
+        Ok(mut stream) => {
+            let read_command = [MAGIC0, MAGIC1, CMD_FREAD];
+            
+            if stream.write(&read_command).is_err() {
+                log!("Failed to send read command");
+                return;
+            }
+
+            if client_send_key(config, &mut stream).is_err() {
+                log!("Failed to send key");
+                return;
+            }
+
+            // generate json file for read request
+            let json_string = format!("{{\"path\":\"{remote_source_file}\"}}");
+            let mut json_string_len = [
+                ((json_string.as_bytes().len() >> 0) & 0xFF) as u8,
+                ((json_string.as_bytes().len() >> 8) & 0xFF) as u8,
+                ((json_string.as_bytes().len() >> 16) & 0xFF) as u8,
+                ((json_string.as_bytes().len() >> 24) & 0xFF) as u8
+            ];
+
+            if stream.write(&json_string_len).is_err() {
+                log!("Failed to send json request length");
+                return;
+            }
+
+            if stream.write(&json_string.as_bytes()).is_err() {
+                log!("Failed to send json request");
+                return;
+            }
+
+            let mut ack_response = [0u8; 4];
+            if stream.read_exact(&mut ack_response).is_err() {
+                log!("Failed to read ack/nack response");
+                return;
+            }
+
+            if ack_response != ACK_SEQUENCE {
+                log!("Bad ACK response");
+                if ack_response == NACK_SEQUENCE {
+                    log!("(NACK)");
+                }
+                return;
+            }
+
+            if stream.read_exact(&mut json_string_len).is_err() {
+                log!("Failed to read json response length");
+                return;
+            }
+
+            let response_len = (json_string_len[0] as usize) |
+                                   (json_string_len[1] as usize) << 8 |
+                                   (json_string_len[2] as usize) << 16 |
+                                   (json_string_len[3] as usize) << 24;
+
+            let mut json_response : Vec<u8> = vec![0u8; response_len];
+            
+            if stream.read_exact(&mut json_response).is_err() {
+                log!("Failed to read json response of length {response_len}");
+                return;
+            }
+
+            let _ = stream.write(&ACK_SEQUENCE);
+
+            let json_response_string = String::from_utf8(json_response.clone()).unwrap();
+            log!("response: {json_response_string}");
+
+            // first, save off raw json for utility reasons
+            let mut file_raw = match File::options().write(true).create(true).truncate(true).open(format!("{local_destination_file}.raw.json")) {
+                Ok(file) => file,
+                Err(why) => {
+                    log!("Failed to open {local_destination_file}.raw.json for writing!");
+                    return;
+                }
+            };
+
+            if file_raw.write(&json_response).is_err() {
+                log!("Failed to write json raw log file.");
+                return;
+            }
+            
+            // convert json string to object
+            let json_object = json::parse(&json_response_string.as_str());
+            if json_object.is_err() {
+                log!("Failed to parse json response:");
+                return;
+            }
+
+            let json_object = json_object.unwrap();
+            let data = match &json_object["data"] {
+                JsonValue::Array(json_array) => {
+                    let mut vectorized : Vec<u8> = vec![];
+                    for element in json_array {
+                        match element {
+                            JsonValue::Number(value) => {
+                                vectorized.push(value.to_string().parse::<i32>().unwrap() as u8);
+                            },
+                            _ => {
+                                log!("Invalid seuquence, skipping");
+                            }
+                        }
+                    }
+
+                    vectorized
+                },
+                _ => "UNSUPPORTED\n".to_string().into()
+            };
+
+            let mut file = match File::options().write(true).create(true).truncate(true).open(local_destination_file) {
+                Ok(file) => file,
+                Err(why) => {
+                    log!("Failed to open {local_destination_file} for writing: {why}");
+                    return;
+                }
+            };
+
+            let _ = file.write(&data);
+        },
+        Err(why) => {
+            log!("Failed to open stream to host.");
+            return;
+        }
+    }
+}
+
+fn handle_client_execute(args: &Vec<String>, config : &JsonValue) {
+    if args.len() != 3 {
+        print_client_usages();
+        return;
+    }
+}
+
+fn handle_client_ping(args: &Vec<String>, config : &JsonValue) {
+    let ping_command = [MAGIC0, MAGIC1, CMD_PING];
+
+    match client_connect(config) {
+        Ok(mut stream) => {
+            match stream.write(&ping_command) {
+                Ok(_) => {
+                    let mut ack_packet = [0u8; ACK_SEQUENCE.len()];
+                    match stream.read(&mut ack_packet) {
+                        Ok(_) => {
+                            match ack_packet {
+                                ACK_SEQUENCE => log!("Ping successful!"),
+                                NACK_SEQUENCE => log!("Received NACK from server"),
+                                _ => log!("received unknown sequence from ping command.")
+                            }
+                        },
+                        Err(why) => {
+                            log!("Failed to read ACK: {why}");
+                        }
+                    }
+                },
+                Err(why) => {
+                    log!("Failed to write ping command to server: {why}");
+                }
+            } 
+        },
+        Err(why) => {
+            log!("Failed to connect to server: {why}");
+        }
+    }
+}
+
+fn print_client_usages() {
+    println!("Invalid command. Usages:");
+    println!("No parameters: start a server using {CONFIG_PATH} settings.");
+    println!("-w local_source_file remote_destination_file [{CONFIG_PATH} is used for target server] | write a file");
+    println!("-r remote_source_file local_destination_file [{CONFIG_PATH} is used for target server] | read a file");
+    println!("-x remote_file [{CONFIG_PATH} is used for target server] | execute a script");
+    println!("-p [{CONFIG_PATH} is used for target server] | ping the server")
+}
+
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
     let path = Path::new(CONFIG_PATH);
     let path_uni = path.display();
     let config;
@@ -300,6 +527,22 @@ fn main() {
     };
 
     log!("KISSAS Settings: {agent_settings}");
+
+    if args.len() > 1 {
+        let client_command = &args[1];
+
+        match client_command.as_str() {
+            "-w" | "-W" => return handle_client_write(&args, &agent_settings),
+            "-r" | "-R" => return handle_client_read(&args, &agent_settings),
+            "-x" | "-X" | "-e" | "-E" => return handle_client_execute(&args, &agent_settings),
+            "-p" | "-P" => return handle_client_ping(&args, &agent_settings),
+            _ => {
+                print_client_usages();
+                return;
+            }
+        }
+    }
+
 
     let listener = match TcpListener::bind(format!("{}:{}", agent_settings["net_config"]["ip"], agent_settings["net_config"]["port"])) {
         Ok(tcp_listener) => tcp_listener,
